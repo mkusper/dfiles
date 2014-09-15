2fb91ebfc8357df87b1d54859b7e876f296ff4ac
mkusper <mkusper@sweptcloud.com>
2014-09-15 07:06:47 -0500

    first commit

---
+++ .emacs.d/elpa/smartrep-20140404.1714/smartrep-autoloads.el
@@ -0,0 +1,18 @@
+;;; smartrep-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("smartrep-pkg.el" "smartrep.el" "test-smartrep.el")
+;;;;;;  (21526 41263 112348 240000))
+
+;;;***
+
+(provide 'smartrep-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; smartrep-autoloads.el ends here
---
+++ .emacs.d/elpa/volatile-highlights-20140812.28/volatile-highlights-autoloads.el
@@ -0,0 +1,18 @@
+;;; volatile-highlights-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("volatile-highlights-pkg.el" "volatile-highlights.el")
+;;;;;;  (21526 41264 663040 635000))
+
+;;;***
+
+(provide 'volatile-highlights-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; volatile-highlights-autoloads.el ends here
